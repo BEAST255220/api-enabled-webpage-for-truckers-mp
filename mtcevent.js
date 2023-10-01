0c8e34384a792784551a66603c4857cd367c6074
Nihal aduvarakkal <72524779+amnihal@users.noreply.github.com>
2023-10-01 20:00:09 +0530

    Update mtcevent.js for update time

--- mtcevent.js
+++ mtcevent.js
@@ -7,7 +7,7 @@ window.onload = async () => {
 async function updateEvents() {
     const Response = await fetch("https://cors-anywhere.jcrbuilds.repl.co/api.truckersmp.com/v2/vtc/55473/events", {
         method: 'GET',
-        Headers: {
+        headers: {
             accept: 'application/json',
             origin: ''
         }
@@ -16,8 +16,10 @@ async function updateEvents() {
     var event = events.response
 
     function DatePassed(date) {
-        const currentDate = new Date();
-        return date < currentDate
+        const currentDateIST = new Date();
+        currentDateIST.setUTCHours(currentDateIST.getUTCHours() - 5); // Add 5 hours for IST
+        currentDateIST.setUTCMinutes(currentDateIST.getUTCMinutes() - 30); // Add 30 minutes for IST
+        return date < currentDateIST;
     }
 
     var Count = 0
@@ -49,25 +51,31 @@ async function updateEvents() {
             </div>
         </div>
         `
-    const NewDiv = document.createElement('div')
-    NewDiv.setAttribute('id', 'event')
-    NewDiv.setAttribute('event-date', event[i].start_at)
-    NewDiv.innerHTML = eventsHTML
-    document.getElementById('events').appendChild(NewDiv)
 
-    Count = Count + 1
-    console.log(Count)
+        
+        const NewDiv = document.createElement('div')
+        NewDiv.setAttribute('id', 'event')
+        NewDiv.setAttribute('event-date', event[i].start_at)
+        NewDiv.innerHTML = eventsHTML
+        document.getElementById('events').appendChild(NewDiv)
 
-    } 
+        Count = Count + 1
+        console.log(Count)
+
+        }
+        
+
+        
 
     
-} 
+    } 
+    
 };
 
 async function aupdateEvents() {
     const Response = await fetch("https://cors-anywhere.jcrbuilds.repl.co/api.truckersmp.com/v2/vtc/55473/events/attending", {
         method: 'GET',
-        Headers: {
+        headers: {
             accept: 'application/json',
             origin: ''
         }
@@ -76,8 +84,10 @@ async function aupdateEvents() {
     var event = events.response
 
     function DatePassed(date) {
-        const currentDate = new Date();
-        return date < currentDate
+        const currentDateIST = new Date();
+        currentDateIST.setUTCHours(currentDateIST.getUTCHours() - 4); // Add 5 hours for IST
+        
+        return date < currentDateIST;
     }
 
     var Count = 0
@@ -85,7 +95,8 @@ async function aupdateEvents() {
     for (var i = 0; i < event.length; i++){
 
         const Passed = (DatePassed(new Date(event[i].start_at)))
-        if (!Passed && Count < 3){
+        
+        if (!Passed && Count < 6){
 
         var banner = event[i].banner
 
@@ -98,7 +109,7 @@ async function aupdateEvents() {
                     
                 </a>
             </div>
-            <p><i class="fa fa-calendar-alt"></i> ${event[i].start_at}<br>
+            <p><i class="fa fa-calendar-alt"></i> ${event[i].start_at} UTC<br>
                 <i class="fa fa-gamepad"></i> ${event[i].game}<br>
                 <i class="fa fa-server"></i> ${event[i].server.name}<br>
                 <i class="fa fa-map"></i> ${event[i].departure.city} > ${event[i].arrive.city} <br>
@@ -118,8 +129,8 @@ async function aupdateEvents() {
     Count = Count + 1
     console.log(Count)
 
-    } 
-
+    }
+    
     
 } 
 };
